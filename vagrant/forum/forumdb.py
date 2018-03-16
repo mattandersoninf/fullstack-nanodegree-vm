9228ab7a1370e57c4c8a44856a900bf4b9b68bc7
matt.anderson.inf <matt.anderson.inf@gmail.com>
2018-03-15 20:50:43 -0400

    added code sections to connect to psql db but there is an error when running from vm, need to fix and test once updated

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
+
+
+DBNAME = "forum"
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc")
+  db.close()
+  return c.fetchall()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("insert into posts values ('%s%')" % content)
+  db.commit()
+  db.close()
+  
+  
