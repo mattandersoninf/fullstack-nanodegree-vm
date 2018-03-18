9d0d3c8d0a5ceb240f261a2742f38b6cdc2b0d24
matt.anderson.inf <matt.anderson.inf@gmail.com>
2018-03-17 22:44:03 -0400

    I understand how the update code works and how to prevent specific spam from trashing my DB, but still need to look further into bleach library and preventing more types of bugs and spam

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,27 +2,40 @@
 
 import datetime
 import psycopg2
+import bleach
 
 
 DBNAME = "forum"
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
-
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select content, time from posts order by time desc")
-  db.close()
-  return c.fetchall()
+    try:
+        """Return all posts from the 'database', most recent first."""
+        DB = psycopg2.connect("dbname=forum")
+        c = DB.cursor()
+        c.execute("SELECT time, content FROM posts ORDER BY time DESC")
+        #c.execute("UPDATE posts SET content = 'cheese' WHERE content LIKE '%spam%';")
+        '''Get all the posts from the database, sorted with the newest first.
+        
+        Returns:
+        A list of dictionaries, where each dictionary has a 'content' key
+        pointing to the post content, and 'time' key pointing to the time
+        it was posted.
+        '''
+        posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+        #for row in c.fetchall())
+        #posts.sort(key=lambda row: row['time'], reverse=True)
+        DB.close()
+        return posts
+    except Exception as e:
+        print("There is something wrong",e)
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("insert into posts values ('%s')" % content)
-  db.commit()
-  db.close()
+    """Add a post to the 'database' with the current timestamp."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    cleaned = bleach.clean(content, strip = True)
+    c.execute("insert into posts values(%s)", (cleaned,))
+    db.commit()
+    db.close()
   
   
-#
